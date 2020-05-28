2c38643967aafe0340407d996ab65f260bdcea82
David Avila Gracia <d.avilag23@gmail.com>
2020-05-28 21:18:53 +0100

    Supporting CLI parameters

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,38 @@
-import { subscribe, sfdxAuthenticate } from './sf';
+import minimist from 'minimist';
+import { subscribe, sfdxAuthenticate, authenticate } from './sf';
 
-sfdxAuthenticate('devorg')
-  .then((auth) => {
-    console.log(auth);
-    subscribe(auth, 'evet');
-  });
+const cliParams = minimist(process.argv.slice(2));
 
-// authenticate(
-//   process.env.SF_USERNAME!,
-//   process.env.SF_PASS!,
-//   process.env.SF_TOKEN!,
-//   process.env.SF_URL!,
-//   process.env.SF_CLIENT_SECRET!,
-//   process.env.SF_CLIENT_ID!,
-// )
-//   .then((authInfo) => {
-//     subscribe(authInfo, process.env.SF_EVENT!);
-//   });
+const event = cliParams.e || cliParams.event;
+const environment = cliParams.env;
+const username = cliParams.u || cliParams.username;
+
+if (!event) {
+  throw Error('Expected Platform Event in the --event parameter');
+}
+
+if (username) {
+  console.info('Authenticating using SFDX config...');
+  sfdxAuthenticate(username)
+    .then((auth) => {
+      subscribe(auth, event);
+    })
+    .catch(() => console.error('Couldn\'t get user information'));
+} else if (environment) {
+  console.info('Authenticating environment variables...');
+  authenticate(
+    process.env.SF_USERNAME!,
+    process.env.SF_PASS!,
+    process.env.SF_TOKEN!,
+    process.env.SF_URL!,
+    process.env.SF_CLIENT_SECRET!,
+    process.env.SF_CLIENT_ID!,
+  )
+    .then((auth) => {
+      subscribe(auth, event);
+    });
+} else {
+  throw Error('Expected --username or --env flag to authenticate with Salesforce');
+}
 
 process.stdin.resume();
